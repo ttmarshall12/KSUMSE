798d898abe068a7cb341e311b3946fe713a6d8a9
ttmarshall12 <t.tracy.marshall@gmail.com>
2016-08-31 22:34:54 -0500

    Final tweaks before demo

--- TCPClient.py
+++ TCPClient.py
@@ -6,13 +6,57 @@ Created on Tue Jul 12 20:30:51 2016
 """
 
 import socket
+import rsa
+import os.path
 #import rsa
 
 TCP_IP = '192.168.0.113'
 HOST = ''
 TCP_PORT = 50021
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = "Hello Mate!"
+
+
+print('Loading server public key...')
+f = open('serverpublic.key', 'r')
+keyfile = f.read()
+serverpubkey = rsa.PublicKey.load_pkcs1(keyfile, 'PEM')
+f.close()
+
+if not os.path.isfile('public.key') or not os.path.isfile('private.key'):
+   
+   print("Gnerating RSA keys...")
+   (pubkey, privkey) = rsa.newkeys(512)
+   
+   f = open('public.key', 'wb')
+   f.write((pubkey.save_pkcs1('PEM')))
+   f.close()
+   
+   f = open('private.key', 'wb')
+   f.write((privkey.save_pkcs1('PEM')))
+   f.close()
+   
+else:
+   
+   print("loading public key...")
+   f = open('public.key', 'r')
+   keyfile = f.read()
+   pubkey = rsa.PublicKey.load_pkcs1(keyfile, 'PEM')
+   f.close()
+   #f.open('public.key','r')
+   #pubkey = f.read()
+   #f.close()
+   
+   print("loading private key...")
+   f = open('private.key', 'r')
+   keyfile = f.read()
+   privkey = rsa.PrivateKey.load_pkcs1(keyfile,'PEM')
+   f.close()
+   #f.open('private.key', 'r')
+   #privkey = f.read()
+   #f.close()
+
+
 
 print('Creating socket...')
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,13 +64,20 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket connecting...')
 s.connect((TCP_IP, TCP_PORT))
 
-print('Socket sent: ' + MESSAGE)
-s.send(MESSAGE.encode())
+
+
+cryptmessage = rsa.encrypt(MESSAGE.encode(),serverpubkey)
+
+print('Socket sent: ' + str(cryptmessage))
+s.send(cryptmessage)
 
 
 data = s.recv(BUFFER_SIZE)
 
-print('Received: ' + str(data))
+decryptedData = rsa.decrypt(data,privkey)
+
+print('Received Enrypted: ' + str(data))
+print('Received Decrypted: ' + str(decryptedData))
 s.close()
 
 #print("Sent data:", data)
--- TCPServer.py
+++ TCPServer.py
@@ -1,10 +1,48 @@
 import socket
 import rsa
+import os.path
 
 SEND_PORT = 50021
 
-print("Gnerating RSA keys...")
-(pubkey, privkey) = rsa.newkeys(512)
+if not os.path.isfile('public.key') or not os.path.isfile('private.key'):
+   
+   print("Gnerating RSA keys...")
+   (pubkey, privkey) = rsa.newkeys(512)
+   
+   f = open('public.key', 'w')
+   f.write(pubkey.save_pkcs1('PEM'))
+   f.close()
+   
+   f = open('private.key', 'w')
+   f.write(privkey.save_pkcs1('PEM'))
+   f.close()
+   
+else:
+   
+   print("loading public key...")
+   f = open('public.key', 'r')
+   keyfile = f.read()
+   pubkey = rsa.PublicKey.load_pkcs1(keyfile, 'PEM')
+   f.close()
+   #f.open('public.key','r')
+   #pubkey = f.read()
+   #f.close()
+   
+   print("loading private key...")
+   f = open('private.key', 'r')
+   keyfile = f.read()
+   privkey = rsa.PrivateKey.load_pkcs1(keyfile,'PEM')
+   f.close()
+   #f.open('private.key', 'r')
+   #privkey = f.read()
+   #f.close()
+   
+
+print("loading client public key...")
+f = open('clientpublic.key', 'r')
+keyfile = f.read()
+clientpubkey = rsa.PublicKey.load_pkcs1(keyfile, 'PEM')
+f.close()
 
 message = 'hello mate!'
 cryptmessage = rsa.encrypt(message,pubkey)
@@ -36,12 +74,16 @@ while 1:
 
     print("Recieving data...")
     data = client.recv(1024)
+   
+    print("Enrypted data: " + str(data))
+    decryptedData = rsa.decrypt(data,privkey)
+    print("Decrypted data: " + str(decryptedData))
+    
+    MESSAGE = "I received your data"
+    
+    encryptedData = rsa.encrypt(MESSAGE.encode(), clientpubkey)
 
-    print("The data was: ")
-    print(data)
-
-    if data:
-        client.send(data)
+    client.send(encryptedData)
 
     client.close()
 
